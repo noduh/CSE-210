03d9e3eeb77a666f3329d3d0a00c44e215285571
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-04-27 20:10:57 -0600

    Completed the Prep 2 assignment including stretch challenges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeLetter;
+        bool gradePassing;
+        string message;
+        bool testForPlus = true;
+        bool testForMinus = true;
+        string plusOrMinus = "";
+        Console.Write("What is your grade percentage? ");
+        float gradePercentage = float.Parse(Console.ReadLine());
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+            gradePassing = true;
+            testForPlus = false;
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+            gradePassing = true;
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+            gradePassing = true;
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+            gradePassing = false;
+        }
+        else
+        {
+            gradeLetter = "F";
+            gradePassing = false;
+            testForPlus = false;
+            testForMinus = false;
+        }
+
+        if (testForPlus && gradePercentage % 10 >= 7)
+        {
+            plusOrMinus = "+";
+        }
+        else if (testForMinus && gradePercentage % 10 < 3)
+        {
+            plusOrMinus = "-";
+        }
+
+        message = $"Your grade is {gradeLetter + plusOrMinus}. ";
+        if (gradePassing)
+        {
+            message += "Great job passing!";
+        }
+        else
+        {
+            message += "Keep trying to bring your grade up!";
+        }
+
+        Console.WriteLine(message);
     }
 }
