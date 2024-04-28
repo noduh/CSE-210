0196e3549bb11a6881bdfc77415437acd6384a2e
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-04-27 18:37:46 -0600

    Started and finished Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,11 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+class Program {
+    static void Main(string[] args) {
+        Console.Write("What is your first name? ");
+        String FirstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String LastName = Console.ReadLine();
+        Console.WriteLine($"\nYour name is {LastName}, {FirstName} {LastName}.");
     }
 }
