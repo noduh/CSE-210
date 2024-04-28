cd1dce2ee97baa4b015d4976a59e713b6a5d21bd
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-04-27 19:12:08 -0600

    Formatting changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        String FirstName = Console.ReadLine();
+        String firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        String LastName = Console.ReadLine();
-        Console.WriteLine($"\nYour name is {LastName}, {FirstName} {LastName}.");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
