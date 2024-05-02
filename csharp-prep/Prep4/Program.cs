ae89c486802f4ee66a41e3ea0300542346d11d7f
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-05-01 23:31:00 -0600

    Finished the core requirements for Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Get the numbers from the user
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<float> numbersFromUser = [];
+        do
+        {
+            Console.Write("Enter number: ");
+            float numberToAdd = float.Parse(Console.ReadLine());
+            numbersFromUser.Add(numberToAdd);
+        } while (numbersFromUser.Last() != 0);
+
+        // Find the total of all the numbers added together
+        float total = 0;
+        foreach (float numberToAdd in numbersFromUser)
+        {
+            total += numberToAdd;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        // Find the average of all the numbers.
+        float average = total / (numbersFromUser.Count - 1);
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the largest number in the list
+        float largestNumber = numbersFromUser[0];
+        foreach (float currentNumber in numbersFromUser)
+        {
+            if (currentNumber > largestNumber)
+            {
+                largestNumber = currentNumber;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
