ea895968390f2cbea1dbef90459574513546e854
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-05-01 22:32:31 -0600

    added a count for number of guesses on the guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,9 +7,11 @@ class Program
         Random random = new Random();
         int randomNumber = random.Next(1, 100);
         bool hasGuessed = false;
+        int numberOfGuesses = 0;
         Console.WriteLine("Guess the random number (from 1 to 100)");
         while (!hasGuessed)
         {
+            numberOfGuesses++;
             Console.Write("What is your guess? ");
             int guess = int.Parse(Console.ReadLine());
             if (guess < randomNumber)
@@ -22,7 +24,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed it in {numberOfGuesses} tries!");
                 hasGuessed = true;
             }
         }
