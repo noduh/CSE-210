a05a522e8856edf732c40638074c6c5dd56818cc
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-05-01 22:28:37 -0600

    Wrote guessing game without stretch challenges (prep 3)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int randomNumber = random.Next(1, 100);
+        bool hasGuessed = false;
+        Console.WriteLine("Guess the random number (from 1 to 100)");
+        while (!hasGuessed)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher\n");
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower\n");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                hasGuessed = true;
+            }
+        }
     }
 }
