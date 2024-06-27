958e3a3ca94ce05f9710da1ea7f8675e539e27e1
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-06-26 21:41:40 -0600

    improved menu

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -17,7 +17,8 @@ class Program
         int userChoice;
         do
         {
-            Console.Write($"{menu}\nChoice: ");
+            Console.Clear();
+            Console.Write($"{menu}\n\nChoice: ");
             userChoice = NonNegativeIntInput();
             switch (userChoice)
             {
@@ -26,24 +27,31 @@ class Program
                     break;
                 case 1:
                     Console.WriteLine("Display Goals");
+                    Console.ReadLine();
                     break;
                 case 2:
                     Console.WriteLine("Create Goal");
+                    Console.ReadLine();
                     break;
                 case 3:
                     Console.WriteLine("Current Score: score");
+                    Console.ReadLine();
                     break;
                 case 4:
                     Console.WriteLine("Mark Complete");
+                    Console.ReadLine();
                     break;
                 case 5:
                     Console.WriteLine("Saving...");
+                    Console.ReadLine();
                     break;
                 case 6:
                     Console.WriteLine("Loading...");
+                    Console.ReadLine();
                     break;
                 default:
                     Console.WriteLine("Bad Choice");
+                    Console.ReadLine();
                     break;
             }
         } while (userChoice != 0);
